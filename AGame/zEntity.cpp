c1113998cc6b7a4b6756a7a430d794391cf4851b
Zachary Tay <zacharytay1994@gmail.com>
2021-02-09 12:36:58 +0800

    commented entity.cpp

--- AGame/zEntity.cpp
+++ AGame/zEntity.cpp
@@ -1,6 +1,14 @@
 #include "zEntity.h"
 #include "Factory.h"
 
+/*______________________________________________________
+	* Brief:	Destroy this instance.
+	*
+	* Access:	public
+	*
+	* Use e.g:	Entity& e1 = Factory::CreateEntity<Com_Position>();
+	*			e1.Destroy();
+________________________________________________________*/
 void Entity::Destroy()
 {
 	// remove from chunk
