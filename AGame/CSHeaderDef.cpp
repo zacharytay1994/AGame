54e69d01d08c8e755d90301213ee7a3ad3939351
Zachary Tay <zacharytay1994@gmail.com>
2021-03-03 13:46:15 +0800

    refactor csheaderdef

---
+++ AGame/CSHeaderDef.cpp
@@ -0,0 +1,20 @@
+#include "CSHeaderDef.h"
+
+/*___________________________________________________________________________________________________________________________________
+	SYSTEM DECLARATIONS																						<<	SYSTEM DECLARATION  >>
+	_________________________________________________________________________________________________________________________________
+	>> basic systems
+		- Sys_DrawSprite
+	>> input
+		- Sys_ArrowKeys
+		- Sys_ArrowKeysTilemap
+	>> tilemap
+		- Sys_Tilemap
+		- Sys_TilemapPosition
+		- Sys_TilePosition
+	>> collision
+		- Sys_BoundingBox
+		- Sys_AABB
+	>> attack
+		- Sys_Projectile
+________________________________________________________________________*/
