82458b49766cf8837098e405b0da3b1f63c1b82f
Zachary Tay <zacharytay1994@gmail.com>
2021-03-04 12:48:42 +0800

    add system data

--- AGame/SceneDeclarations.h
+++ AGame/SceneDeclarations.h
@@ -8,6 +8,12 @@
 #include "ResourceManager.h"
 #include <string>
 
+struct testdata {
+	int i;
+};
+void testfunction(Sys_Tilemap* t, testdata td) {
+	t->i = td.i;
+}
 /*!___________________________________________________________________
 	TEST SCENE - Created By : Zac
 _____________________________________________________________________*/
@@ -16,9 +22,10 @@ struct TestScene : public Scene {
 	Member Variables
 	________________________________*/
 	std::string test = "hello";
-	eid e2 = -1;
+	eid player = -1;
 	//Com_Tilemap tile;
 	eid tilemap = -1;
+	Factory::SpriteData data{ "test2", 1, 8, 8, 0.1f, 100.0f, 200.0f };
 	/*
 	Initialize Override (optional)
 	________________________________*/
@@ -27,54 +34,47 @@ struct TestScene : public Scene {
 		std::cout << sizeof(Com_Tilemap) << std::endl;
 
 		tilemap = Factory::Instance().FF_Tilemap("tilemap", "c_test.txt", "t_test.txt");
-		/*Entity& testing = Factory::Instance()[tilemap];
-		Com_Tilemap& test = Factory::Instance()[tilemap].Get<Com_Tilemap>();*/
-		e2 = Factory::Instance().FF_SpriteMovableTile<Com_ArrowKeysTilemap>({ "test2", 1, 8, 8, 0.1f, 100.0f, 200.0f }, tilemap, 0, 0);
-		//Com_TilemapRef& ref = Factory::Instance()[e2].Get<Com_TilemapRef>();
-		//Com_Sprite& test2 = Factory::Instance()[e2].Get<Com_Sprite>();
-		//e2 = Factory::Instance().FF_Sprite({ "test2", 1, 8, 8, 0.1f, 100.0f, 200.0f }, 0, 0);
-
-		//tilemap = Factory::Instance().FF_Tilemap("tilemap", "c_test.txt", "t_test.txt");
-		/*e2 = Factory::Instance().FF_Sprite({ "test2", 1, 8, 8, 0.1f, 100.0f, 200.0f }, 0, 0);
-		Entity& entitytest = Factory::Instance()[e2];
-		Com_Sprite& cs = entitytest.Get<Com_Sprite>();
-		Com_Position& pos = entitytest.Get<Com_Position>();*/
+		//player = Factory::Instance().FF_SpriteMovableTile<Com_ArrowKeysTilemap>(data, tilemap, 0, 0);
 	}
 	/*
 	Update Override (optional)
 	________________________________*/
 	void Update(const float& dt) override {
 		//Entity& testing = Factory::Instance()[tilemap];
-		if (AEInputCheckCurr('L')) {
-			SceneManager::Instance().ChangeScene("Test Scene 2");
+		//if (AEInputCheckTriggered('E')) {
+		//}
+
+		//if (AEInputCheckCurr('L')) {
+		//	SceneManager::Instance().ChangeScene("Test Scene 2");
+		//}
+		if (AEInputCheckTriggered('N')) {
+			std::cout << SystemDatabase::Instance().GetSystem<Sys_Tilemap>().i++ << std::endl;
+			std::cout << SystemDatabase::Instance().GetSystem<Sys_Tilemap>().i << std::endl;
 		}
 		if (AEInputCheckTriggered('P')) {
-			e2 = Factory::Instance().FF_SpriteTile({ "test2", 1, 8, 8, 0.2f, 50.0f, 80.0f }, tilemap, 3.0f, 0.0f);
-			//Factory::Instance().GetEntity(e2).Get<Com_Sprite>()._frame_interval -= dt;
-			//Com_Sprite& s = e2->Get<Com_Sprite>();
+			player = Factory::Instance().FF_SpriteTile({ "test2", 1, 8, 8, 0.2f, 50.0f, 80.0f }, tilemap, 3.0f, 0.0f);
+			//Factory::Instance().GetEntity(player).Get<Com_Sprite>()._frame_interval -= dt;
+			//Com_Sprite& s = player->Get<Com_Sprite>();
 		}
 		if (AEInputCheckTriggered('O')) {
-			Factory::Instance()[e2].AddComponent<Com_ArrowKeysTilemap>();
+			Factory::Instance()[player].AddComponent<Com_ArrowKeysTilemap>();
 		}
-		if (AEInputCheckTriggered('M')) {
-			Entity& test = Factory::Instance()[tilemap];
-			Factory::Instance()[tilemap].AddComponent<Com_Position>();
-			Factory::Instance()[tilemap].AddComponent<Com_ArrowKeys>();
-		}
-		if (AEInputCheckCurr('I')) {
-			Entity& e = Factory::Instance().GetEntity(e2);
-			e.AddComponent<Com_Tilemap>();
-			ResourceManager::Instance().GetResource(e.Get<Com_Tilemap>()._texture, e.Get<Com_Tilemap>()._mesh, "tilemap", 4, 4, 16);
-			ResourceManager::Instance().ReadTilemapTxt("t_test.txt", e.Get<Com_Tilemap>());
-			ResourceManager::Instance().ReadFloorMapTxt("c_test.txt", e.Get<Com_Tilemap>());
-			Com_Tilemap& tilemap = e.Get<Com_Tilemap>();
-			e.Get<Com_Tilemap>()._scale_x = 100.0f;
-			e.Get<Com_Tilemap>()._scale_y = 100.0f;
-			e.Get<Com_Tilemap>()._initialized = true;
-		}
-		if (AEInputCheckTriggered('R')) {
-			SceneManager::Instance().RestartScene();
-		}
+		//if (AEInputCheckTriggered('U')) {
+		//	player = Factory::Instance().FF_Sprite({ "test2", 1, 8, 8, 0.2f, 50.0f, 80.0f }, 3.0f, 0.0f);
+		//	Factory::Instance()[player].AddComponent<Com_ArrowKeys>();
+		//}
+		//if (AEInputCheckTriggered('M')) {
+		//	Entity& test = Factory::Instance()[tilemap];
+		//	Factory::Instance()[tilemap].AddComponent<Com_Position>();
+		//	Factory::Instance()[tilemap].AddComponent<Com_ArrowKeys>();
+		//}
+		//if (AEInputCheckCurr('I')) {
+		//	int e1 =
+		//		tilemap = Factory::Instance().FF_Tilemap("tilemap", "c_test.txt", "t_test.txt");
+		//}
+		//if (AEInputCheckTriggered('R')) {
+		//	SceneManager::Instance().RestartScene();
+		//}
 	}
 	/*
 	Exit Override (optional)
