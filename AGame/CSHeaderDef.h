ef8a774679a70bfe23ca11c797d3cd1be5e33b6d
Ryuki1998 <73224169+Ryuki1998@users.noreply.github.com>
2021-03-04 11:33:52 +0800

    Added pathfinding in csheaderdef.h

--- AGame/CSHeaderDef.h
+++ AGame/CSHeaderDef.h
@@ -598,4 +598,95 @@ void MapCreate(Com_Node& ode)
 	ode.nodeStart = &ode.nodes[(height / 2) * width + 1];
 	ode.nodeEnd = &ode.nodes[(height / 2) * width + width - 2];
 }
+
+bool Solve_AStar(Com_Node& ode)
+{
+	int width = ode.nMapWidth;
+	int height = ode.nMapHeight;
+	// Reset Navigation Graph - default all node states
+	for (int x = 0; x < width; x++)
+		for (int y = 0; y < height; y++)
+		{
+			ode.nodes[y * width + x].bVisited = false;
+			ode.nodes[y * width + x].fGlobalGoal = INFINITY;
+			ode.nodes[y * width + x].fLocalGoal = INFINITY;
+			ode.nodes[y * width + x].parent = nullptr;	// No parents
+		}
+
+	auto distance = [](NodeForPathFinding* a, NodeForPathFinding* b) // For convenience
+	{
+		return sqrtf((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y));
+	};
+
+	auto heuristic = [distance](NodeForPathFinding* a, NodeForPathFinding* b) // So we can experiment with heuristic
+	{
+		return distance(a, b);
+	};
+
+	// Setup starting conditions
+	NodeForPathFinding* nodeCurrent = ode.nodeStart;
+	ode.nodeStart->fLocalGoal = 0.0f;
+	ode.nodeStart->fGlobalGoal = heuristic(ode.nodeStart, ode.nodeEnd);
+
+	// Add start node to not tested list - this will ensure it gets tested.
+	// As the algorithm progresses, newly discovered nodes get added to this
+	// list, and will themselves be tested later
+	list<NodeForPathFinding*> listNotTestedNodes;
+	listNotTestedNodes.push_back(ode.nodeStart);
+
+	// if the not tested list contains nodes, there may be better paths
+	// which have not yet been explored. However, we will also stop 
+	// searching when we reach the target - there may well be better
+	// paths but this one will do - it wont be the longest.
+	while (!listNotTestedNodes.empty() && nodeCurrent != ode.nodeEnd)// Find absolutely shortest path // && nodeCurrent != nodeEnd)
+	{
+		// Sort Untested nodes by global goal, so lowest is first
+		listNotTestedNodes.sort([](const NodeForPathFinding* lhs, const NodeForPathFinding* rhs) { return lhs->fGlobalGoal < rhs->fGlobalGoal; });
+
+		// Front of listNotTestedNodes is potentially the lowest distance node. Our
+		// list may also contain nodes that have been visited, so ditch these...
+		while (!listNotTestedNodes.empty() && listNotTestedNodes.front()->bVisited)
+			listNotTestedNodes.pop_front();
+
+		// ...or abort because there are no valid nodes left to test
+		if (listNotTestedNodes.empty())
+			break;
+
+		nodeCurrent = listNotTestedNodes.front();
+		nodeCurrent->bVisited = true; // We only explore a node once
+
+
+		// Check each of this node's neighbours...
+		for (auto nodeNeighbour : nodeCurrent->vecNeighbours)
+		{
+			// ... and only if the neighbour is not visited and is 
+			// not an obstacle, add it to NotTested List
+			if (!nodeNeighbour->bVisited && nodeNeighbour->bObstacle == 0)
+				listNotTestedNodes.push_back(nodeNeighbour);
+
+			// Calculate the neighbours potential lowest parent distance
+			float fPossiblyLowerGoal = nodeCurrent->fLocalGoal + distance(nodeCurrent, nodeNeighbour);
+
+			// If choosing to path through this node is a lower distance than what 
+			// the neighbour currently has set, update the neighbour to use this node
+			// as the path source, and set its distance scores as necessary
+			if (fPossiblyLowerGoal < nodeNeighbour->fLocalGoal)
+			{
+				nodeNeighbour->parent = nodeCurrent;
+				nodeNeighbour->fLocalGoal = fPossiblyLowerGoal;
+
+				// The best path length to the neighbour being tested has changed, so
+				// update the neighbour's score. The heuristic is used to globally bias
+				// the path algorithm, so it knows if its getting better or worse. At some
+				// point the algo will realise this path is worse and abandon it, and then go
+				// and search along the next best path.
+				nodeNeighbour->fGlobalGoal = nodeNeighbour->fLocalGoal + heuristic(nodeNeighbour, ode.nodeEnd);
+			}
+		}
+	}
+
+	return true;
+}
+
+
 };
