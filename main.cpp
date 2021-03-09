f4b16f752cb3e86d00f0d946a12218e8f4d52f84
Zachary Tay <zacharytay1994@gmail.com>
2021-03-09 10:19:10 +0800

    engine proof

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@
 //}
 
 // ---------------------------------------------------------------------------
-// main
+// main - ENGINE PROOF
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
