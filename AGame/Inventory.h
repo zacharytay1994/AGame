1ec4e966361528576c80339d1004aabf3abed64d
Wilfred Ng <wnjh98@gmail.com>
2021-03-25 10:00:35 +0800

    edits

--- AGame/Inventory.h
+++ AGame/Inventory.h
@@ -20,9 +20,9 @@ class Inventory
 		bool Inventory_SetWeaponUnlocked(std::string const& name);
 		bool Inventory_CheckWeaponUnlocked(std::string const& name) const;
 
-		#if defined(DEBUG) | defined(_DEBUG)
+		//#if defined(DEBUG) | defined(_DEBUG)
 			void Inventory_PrintCurrentWeapon() const;
-		#endif
+		//#endif
 
 	private:
 		std::map<std::string, Weapon*> inventory_weapon;
