f46d155f98b89fec4c9105836dc0c5a78ffe5597
Ryuki1998 <73224169+Ryuki1998@users.noreply.github.com>
2021-04-10 17:22:21 +0800

    Boss is done

--- AGame/Scene_Opening.h
+++ AGame/Scene_Opening.h
@@ -1,4 +1,23 @@
 #pragma once
 #include "SceneDeclarations.h"
 
-struct Opening : public Scene {};
+struct Opening : public Scene {
+	eid i{ -1 };
+	eid main{ -1 };
+	eid _settings{ -1 };
+	eid _change_scene{ -1 };
+	eid load{ -1 };
+	Factory::SpriteData data1{ "menubackground" };
+	Factory::SpriteData data2{ "buttonsurface" };
+	Factory::SpriteData data3{ "button1" };
+	Factory::SpriteData data4{ "button2" };
+	Factory::SpriteData data5{ "button3" };
+	Factory::SpriteData data6{ "transparent" };
+	Factory::SpriteData data7{ "gamelogo" };
+	Factory::SpriteData buttonbg{ "buttonsbg.png", 1.0f, 1.0f, 1, 1, 1, 1.0f, 0 };
+	Vec2i passin[5] = { {0,3},{4,7},{0,0},{0,0},{0,0} };
+	Factory::SpriteData button{ "buttonsprite.png", 1.0f, 1.0f, 3, 3, 8, 0.1f, 0, passin };
+	Vec2i passin4[5] = { {0,0},{1,1},{0,0},{0,0},{0,0} };
+	Factory::SpriteData buttonload{ "background2.png", 2.0f, 1.0f, 2, 1, 2, 0.05f, 0, passin4 };
+	bool last = false;
+};
